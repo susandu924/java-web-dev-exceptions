4cb3eec01912a1fd69c9309429248efea1acd327
Susan <susandu924@gmail.com>
2022-09-28 16:32:21 -0500

    exercise-exceptions

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,5 +1,7 @@
 package org.launchcode.exercises;
 
+import org.launchcode.Temperature.TemperatureException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,17 +17,46 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+//        System.out.println(Divide(12,2));
         // Test out your CheckFileExtension() function!
+
+       for(Map.Entry<String, String>student: studentFiles.entrySet()){
+           System.out.println(student.getKey() + ", " + CheckFileExtension((student.getValue())));
+       }
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static int Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Can't divide by zero!");
+            } catch (ArithmeticException a) {
+                a.printStackTrace();
+            }
+            System.out.println("Value is zero");
+        }
+            int z = x / y;
+            return z;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+        int grade = 0;
+
+        if (fileName == "" || fileName == null) {
+            grade -= -1;
+            try {
+                throw new Exception("Invalid file! Minus one point.");
+            } catch (Exception a) {
+                a.printStackTrace();
+            }
+            System.out.println();
+        }
+        if (fileName.contains(".java")) {
+            grade = +1;
+        } else if (!fileName.contains(".java")) {
+            grade += 0;
+        }
+        return grade;
+        }
     }
 
-}
+
